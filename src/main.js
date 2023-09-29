9584e5d2608b9aff2ed822926ba8bd55eb7d7d75
Matheus Cardoso <mattheuscardoso@aol.com>
2023-09-29 16:12:31 -0300

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify/lib'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+const opts = {
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+}
+
 new Vue({
+  vuetify: new Vuetify(opts),
   render: h => h(App),
 }).$mount('#app')
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: true,
+  chainWebpack: (config) => {
+    config
+      .plugin("VuetifyLoaderPlugin")
+      .use(VuetifyLoaderPlugin)
+      .tap(() => [
+        {
+          match(originalTag, { kebabTag, camelTag }) {
+            if (kebabTag.startsWith("v-")) {
+              return [camelTag, `import { ${camelTag} } from "vuetify/lib"`];
+            }
+          },
+        },
+      ]);
+
+  }
 })
+
